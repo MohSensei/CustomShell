6f62e5ee671c04c12805093346f2359d5ecaefbc
Mohammed <74876877+MohSensei@users.noreply.github.com>
2024-08-03 16:14:22 -0400

    Fixing merging problems

--- myshell.c
+++ myshell.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-<<<<<<< HEAD
 #include <string.h>
 #include <stdlib.h>
 
@@ -42,23 +41,3 @@ int main()
 
     return 0;
 }
-=======
-#include <signal.h>
-
-int main()
-{
-    signal(SIGINT, SIG_IGN);
-    char command[1024]; // Buffer to store use input
-
-    while (1)
-    {                        // infinite loop to keep the shell running
-        printf("myshell> "); // Prompt for input
-        if (fgets(command, sizeof(command), stdin) == NULL)
-            break; // Exit if Ctrl+D is pressed
-
-        printf("You enetered: %s", command); // Echo back the input
-    }
-
-    return 0;
-}
->>>>>>> origin/main
